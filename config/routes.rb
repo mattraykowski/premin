8b45e62c8cae807a780f91442d51ba962d00f29d
Matt Raykowski <matt.raykowski@gmail.com>
2013-07-18 14:52:51 -0500

    Added dashboard placeholder and demo of subdomain roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Premin::Application.routes.draw do
+  get "dashboard/index"
+
   devise_for :users
 
   get "home/index"
+
+  constraints(Subdomain) do
+    match "/" => "dashboard#index"
+  end
 
   root :to => 'home#index'
 
