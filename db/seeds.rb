22377e2797e4bb752a5b1818d96464034779dcba
Matt Raykowski <matt.raykowski@gmail.com>
2013-07-26 13:39:45 -0500

    Added a user for a basic demo seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "matt.raykowski@gmail.com", name: "Matt Raykowski", account_name: "My Demo School", account_subdomain: "demo", password: "changeme", password_confirmation: "changeme")
